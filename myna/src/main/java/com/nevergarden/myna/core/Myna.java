b9e6917f86b689d913a385f5f305071da5272262
nevergarden <nevergarden.fa@gmail.com>
2022-08-06 00:01:26 +0430

    add: config to initialization

--- myna/src/main/java/com/nevergarden/myna/core/Myna.java
+++ myna/src/main/java/com/nevergarden/myna/core/Myna.java
@@ -1,10 +1,14 @@
 package com.nevergarden.myna.core;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.SurfaceHolder;
 
+import com.nevergarden.myna.R;
 import com.nevergarden.myna.events.Event;
 import com.nevergarden.myna.events.EventDispatcher;
 import com.nevergarden.myna.events.EventListener;
@@ -14,11 +18,13 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class Myna extends GLSurfaceView {
     public final static String TAG = "Myna";
+
     public final EventDispatcher eventDispatcher;
-    private MynaConfig config;
+    private final MynaConfig config = new MynaConfig();
 
     public Myna(Context context, AttributeSet attrs) {
         super(context, attrs);
+
         this.eventDispatcher = new EventDispatcher();
         this.eventDispatcher.addEventListener(Event.CONTEXT_CREATE, new EventListener() {
             @Override
@@ -33,25 +39,60 @@ public class Myna extends GLSurfaceView {
             }
         });
 
+        this.setConfig(attrs);
+
         this.setRenderer(new com.nevergarden.myna.gfx.Renderer(this));
         this.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
         this.eventDispatcher.dispatchEventWith(Event.CONTEXT_CREATE);
     }
 
-    public void step() {
-        Log.d(TAG, "Step");
-    }
+    public void step() {}
 
     public void render(GL10 gl10) {
+        /*
         Random r = new Random();
         float red = r.nextFloat();
         float green = r.nextFloat();
         float blue = r.nextFloat();
-        gl10.glClearColor(red, green, blue, 1);
+        gl10.glClearColor(red, green, blue, 0.2f);
+
+         */
         this.requestRender();
     }
 
-    public void setConfig(String configData) {
-        this.config = MynaConfigBuilder.create(configData);
+    public void setConfig(AttributeSet configData) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(configData, new int[]{R.attr.myna_pixelFormat});
+        int id = typedArray.getInteger(0, 0);
+        typedArray.recycle();
+        MynaPixelFormat pixelFormat = MynaPixelFormat.fromId(id);
+        this.config.setPixelFormat(pixelFormat);
+
+        setEGLConfigChooser(
+                this.config.getRedSize(),
+                this.config.getRedSize(),
+                this.config.getBlueSize(),
+                this.config.getAlphaSize(),
+                this.config.getDepthSize(),
+                this.config.getAlphaSize()
+        );
+        SurfaceHolder holder = getHolder();
+        switch (this.config.getPixelFormat()) {
+            case OPAQUE: holder.setFormat(PixelFormat.OPAQUE); break;
+            case RGBX_8888: holder.setFormat(PixelFormat.RGBX_8888); break;
+            case RGBA_8888: holder.setFormat(PixelFormat.RGBA_8888); break;
+            case RGB_565: holder.setFormat(PixelFormat.RGB_565); break;
+            case RGB_888: holder.setFormat(PixelFormat.RGB_888); break;
+            case TRANSPARENT: holder.setFormat(PixelFormat.TRANSPARENT); break;
+            case UNKNOWN: holder.setFormat(PixelFormat.UNKNOWN); break;
+        }
+
+        if(this.config.getAlphaSize() != 0)
+            this.setZOrderOnTop(true);
+
+        Log.d(TAG, "r:" + this.config.getRedSize() +
+                "g:" + this.config.getGreenSize() +
+                "b:" + this.config.getBlueSize() +
+                "a:" + this.config.getAlphaSize()
+        );
     }
 }
--- myna/src/main/java/com/nevergarden/myna/core/MynaConfig.java
+++ myna/src/main/java/com/nevergarden/myna/core/MynaConfig.java
@@ -1,10 +1,87 @@
 package com.nevergarden.myna.core;
 
+enum MynaPixelFormat {
+    OPAQUE(0),
+    RGBX_8888(1),
+    RGBA_8888(2),
+    RGB_565(3),
+    RGB_888(4),
+    TRANSPARENT(5),
+    UNKNOWN(6);
+
+    int id;
+
+    MynaPixelFormat(int id) {
+        this.id = id;
+    }
+
+    static MynaPixelFormat fromId(int id) {
+        for (MynaPixelFormat f : values()) {
+            if (f.id == id) return f;
+        }
+        throw new IllegalArgumentException();
+    }
+}
+
 class MynaConfig {
     public int width = 800;
     public int height = 600;
 
-    public int redSize = 8;
-    public int greenSize = 8;
-    public int blueSize = 8;
+    private int redSize = 8;
+    private int greenSize = 8;
+    private int blueSize = 8;
+    private int alphaSize = 8;
+    private int depthSize = 0;
+    private int stencilSize = 0;
+
+    private MynaPixelFormat pixelFormat;
+
+    public MynaConfig() {}
+
+    public MynaPixelFormat getPixelFormat() {
+        return pixelFormat;
+    }
+
+    public void setPixelFormat(MynaPixelFormat pixelFormat) {
+        this.pixelFormat = pixelFormat;
+        switch (this.pixelFormat) {
+            case OPAQUE:
+            case RGB_888:
+            case UNKNOWN:
+                this.redSize = this.greenSize = this.blueSize = 8; this.alphaSize = 0;
+                break;
+            case RGB_565:
+                this.redSize = this.blueSize = 5; this.greenSize = 6; this.alphaSize = 0;
+                break;
+            case RGBA_8888:
+            case RGBX_8888:
+            case TRANSPARENT:
+                this.redSize = this.greenSize = this.blueSize = this.alphaSize = 8;
+                break;
+        }
+    }
+
+    public int getRedSize() {
+        return redSize;
+    }
+
+    public int getGreenSize() {
+        return greenSize;
+    }
+
+    public int getBlueSize() {
+        return blueSize;
+    }
+
+    public int getAlphaSize() {
+        return alphaSize;
+    }
+
+    public int getDepthSize() {
+        return depthSize;
+    }
+
+    public int getStencilSize() {
+        return stencilSize;
+    }
 }
